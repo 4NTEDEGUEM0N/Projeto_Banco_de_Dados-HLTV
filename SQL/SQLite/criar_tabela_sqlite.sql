240a41bc38913fca1e7bf376e45fad75f5bdb350
4NTEDEGUEM0N <solbrilhan@gmail.com>
2024-09-09 16:37:11 -0300

    minor fix

--- SQL/SQLite/criar_tabela_sqlite.sql
+++ SQL/SQLite/criar_tabela_sqlite.sql
@@ -1,9 +1,9 @@
+BEGIN TRANSACTION;
+
 CREATE TABLE alembic_version (
     version_num VARCHAR(32) NOT NULL, 
     CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
 );
-
--- Running upgrade  -> 5502403bc5f6
 
 CREATE TABLE evento (
     id INTEGER NOT NULL, 
@@ -144,5 +144,5 @@ CREATE TABLE time_partida (
     FOREIGN KEY(time_id) REFERENCES time (id)
 );
 
-INSERT INTO alembic_version (version_num) VALUES ('5502403bc5f6') RETURNING version_num;
+COMMIT;
 
